4155b7ffe4811e80fcdad16cc58871277cd91d4e
Vollstrecker <werner@vollstreckernet.de>
2021-03-13 13:06:31 +0100

    Added option to set the aliases from install insetad of doing this in the find modules

--- packages/googletest.cmake
+++ packages/googletest.cmake
@@ -3,4 +3,8 @@ CmDaB_declare (
 	GIT_REPOSITORY https://github.com/google/googletest.git
 	TESTS_ENABLE gtest_build_tests gmock_build_tests
 	GENERIC_OPTIONS_FALSE INSTALL_GTEST
+	ALIASES GTest::gmock gmock
+			GTest::gmock_main gmock_main
+			GTest::gtest gtest
+			GTest::gtest_main gtest_main
 )
