60401f17d3149cc74806169d37bb6ec599fb8a48
Vollstrecker <werner@vollstreckernet.de>
2021-03-13 12:28:22 +0100

    Use only the GTest-CONFIG* and issue a warning when dev's still use FindGTest.

--- modules/FindGTest.cmake
+++ modules/FindGTest.cmake
@@ -1,31 +1,16 @@
+message (AUTHOR_WARNING "You shouldnt' use FindGTest anymore. They ship a config that provides gtest and gmock for you.")
+
 if (NOT GTest_DIR)
-	CmDaB_include_orig (FindGTest)
+	find_package (GTest CONFIG)
 endif()
 
-if (GTEST_FOUND AND NOT GTest_DIR STREQUAL "CmDaB_BUILD")
-	if (CMAKE_VERSION VERSION_LESS 3.20)
-		if (CMAKE_VERSION VERSION_GREATER_EQUAL 3.18)
-			add_library (GTest::gtest ALIAS GTest::GTest)
-		else ()
-			# The following two blocks replicate the original FindThreads
-			add_library (GTest::gtest INTERFACE IMPORTED)
-			get_target_property (gtest_includes GTest::GTest INTERFACE_INCLUDE_DIRECTORIES)
-			get_target_property (gtest_location GTest::GTest INTERFACE_LINK_LIBRARIES)
-
-			set_target_properties(GTest::gtest PROPERTIES
-				INTERFACE_INCLUDE_DIRECTORIES ${gtest_includes}
-				INTERFACE_LINK_LIBRARIES ${gtest_location}
-			)
-		endif()
-	endif()
-else ()
+if (NOT GTest_FOUND AND NOT GTest_DIR STREQUAL "CmDaB_BUILD")
 	if (GTest_DIR STREQUAL "CmDaB_BUILD")
 		CmDaB_install (googletest)
 	else()
 		message (STATUS "Downloading googletest...")
 		CmDaB_install (googletest)
 		message (STATUS "Downloading googletest...Done")
-		set (GTEST_FOUND TRUE)
 
 		set (GTest_DIR "CmDaB_BUILD"
 			CACHE
@@ -35,7 +20,6 @@ else ()
 		)
 	endif()
 
-	add_library (GTest::gtest ALIAS gmock)
-	set (GTEST_FOUND TRUE)
+	set (GTest_FOUND TRUE)
 	include (GoogleTest)
 endif()
