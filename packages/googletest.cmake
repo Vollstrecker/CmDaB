68aa42fdeb033c4ad828ecdc11cf2211fd385669
Vollstrecker <werner@vollstreckernet.de>
2021-12-31 06:46:36 +0100

    googletest is now using main instead of master

--- packages/googletest.cmake
+++ packages/googletest.cmake
@@ -1,6 +1,7 @@
 CmDaB_declare (
 	googletest
 	GIT_REPOSITORY https://github.com/google/googletest.git
+	GIT_TAG main
 	TESTS_ENABLE gtest_build_tests gmock_build_tests
 	GENERIC_OPTIONS_FALSE INSTALL_GTEST
 	ALIASES GTest::gmock gmock
